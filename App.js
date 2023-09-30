215ce69a479ffa45bbd725a10112f365e7bfe2fb
SUNWOO <elecsog@naver.com>
2023-09-30 20:47:27 +0900

    Add FriendList

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/Header';
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import Profile from './src/Profile';
+import { friendProfiles, myProfile } from './src/data';
+import Margin from "./src/Margin";
+import Division from './src/Division';
+import FriendSection from './src/FriendSection';
+import FriendList from './src/FriendList';
+import { useState } from 'react';
+
+const statusBarHeight = getStatusBarHeight(true);
+const bottomSpace = getBottomSpace();
 
 export default function App() {
+
+  const [isOpened, setIsOpened] = useState(true);
+
+
+  const onPressArrow = () => {
+    
+    setIsOpened(!isOpened)
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+
+      <Margin height={10} />
+
+      <Profile 
+        uri={myProfile.uri}
+        name={myProfile.name} 
+        introduction={myProfile.introduction}
+      />
+
+      <Margin height={15}/>
+
+      <Division />
+      <Margin height={12}/>
+      <FriendSection
+        friendProfileLen = {friendProfiles.length}
+        onPressArrow= {onPressArrow}
+        isOpened = {isOpened}
+      />
+     
+      <FriendList
+        data = {friendProfiles}
+        isOpened = {isOpened}
+      /> 
+
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop : statusBarHeight,
+    paddingHorizontal : 15
   },
 });
---
+++ src/FriendList.js
@@ -0,0 +1,80 @@
+import { ScrollView, View } from "react-native"
+import Profile from "./Profile"
+import { getBottomSpace } from "react-native-iphone-x-helper"
+import Margin from "./Margin";
+
+
+const bottomSpace = getBottomSpace();
+
+export default (props) => {
+
+    /**
+     * Case 1 . 상황연산자
+     */
+    // return props.isOpened ? (
+    //     <ScrollView showsVerticalScrollIndicator = {false} contentContainerStyle={{paddingBottom : bottomSpace}}>
+    //         {props.data.map((item, index) => (
+    //             <View key= {index}>
+    //                 <Profile
+                        
+    //                     uri={item.uri}
+    //                     name={item.name} 
+    //                     introduction={item.introduction}
+
+    //                 />    
+    //                 <Margin height = {13}/>
+    //             </View>
+                
+    //         ))}
+    //     </ScrollView>
+    // ) : null;
+
+
+    /**
+     * Case 2 . if 문으로 예외 처리
+     * 
+     */
+
+    // if(!props.isOpened) return null;
+    // return (
+    //     <ScrollView showsVerticalScrollIndicator = {false} contentContainerStyle={{paddingBottom : bottomSpace}}>
+    //         {props.data.map((item, index) => (
+    //             <View key= {index}>
+    //                 <Profile
+                        
+    //                     uri={item.uri}
+    //                     name={item.name} 
+    //                     introduction={item.introduction}
+
+    //                 />    
+    //                 <Margin height = {13}/>
+    //             </View>
+                
+    //         ))}
+    //     </ScrollView>
+    // ) 
+
+    /**
+     * 
+     * Case 3. && 이용
+     * 
+     */
+
+    return props.isOpened && (
+        <ScrollView showsVerticalScrollIndicator = {false} contentContainerStyle={{paddingBottom : bottomSpace}}>
+           {props.data.map((item, index) => (
+               <View key= {index}>
+                   <Profile
+                        
+                        uri={item.uri}
+                        name={item.name} 
+                        introduction={item.introduction}
+
+                    />    
+                    <Margin height = {13}/>
+                </View>
+                
+           ))}
+        </ScrollView>
+    )
+}
